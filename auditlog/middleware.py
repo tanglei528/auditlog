11b14c345af93161c11f2a656f62cbb516af409a
song.gao <song.gao@venusource.com>
2014-06-20 18:50:52 +0800

    Fix tox check errors

--- auditlog/middleware.py
+++ auditlog/middleware.py
@@ -22,40 +22,41 @@ class AuditMiddleware(base.Middleware):
 
         super(AuditMiddleware, self).__init__(application)
         self._audit_methods = audit_methods.split(",")
-        self.connection = impl_mongodb.MongoDBStorage().get_connection(cfg.CONF)
+        self.storage = impl_mongodb.MongoDBStorage()
+        self.connection = self.storage.get_connection(cfg.CONF)
 
     def process_request(self, req):
         _need_audit = req.method in self._audit_methods
         if _need_audit:
-           user_id = req.headers.get('X-User-Id','unknown')
-           tenant_id = req.headers.get('X-Tenant-Id','unknown')
-           path = req.path
-           try:
-               rid = vm.Resource.parse_url(path).rid
-           except:
-               rid = None
-           method = req.method
-           status_code = None
-           begin_at = timeutils.utcnow()
-           end_at = None
-           content = req.body
-           self._log = models.AuditLog(user_id, tenant_id, rid, path, method,
-                           status_code, begin_at, end_at, 
-                           content)
+            user_id = req.headers.get('X-User-Id', 'unknown')
+            tenant_id = req.headers.get('X-Tenant-Id', 'unknown')
+            path = req.path
+            try:
+                rid = vm.Resource.parse_url(path).rid
+            except Exception:
+                rid = None
+            method = req.method
+            status_code = None
+            begin_at = timeutils.utcnow()
+            end_at = None
+            content = req.body
+            self._log = models.AuditLog(user_id, tenant_id, rid, path, method,
+                                        status_code, begin_at, end_at,
+                                        content)
         else:
             self._log = None
 
     def process_response(self, response):
-        
+
         if self._log is not None:
-           self._log.status_code = response.status_int
-           self._log.end_at = timeutils.utcnow()
-           self._store_log(self._log)
-           self._log = None
+            self._log.status_code = response.status_int
+            self._log.end_at = timeutils.utcnow()
+            self._store_log(self._log)
+            self._log = None
         return response
 
     def _store_log(self, log):
         try:
             self.connection.create_auditlog(log.as_dict())
         except Exception as e:
-            LOG.error("Store audit log error : %s",e)
+            LOG.error("Store audit log error : %s", e)
--- auditlog/storage/impl_mongodb.py
+++ auditlog/storage/impl_mongodb.py
@@ -2,35 +2,37 @@ import pymongo
 import weakref
 
 from auditlog.storage import base
-from ceilometer.openstack.common import network_utils
 
 from oslo.config import cfg
 
 
+cfg.CONF.import_opt(
+    'auditlog_connection',
+    'auditlog.storage',
+    group="database")
 
-cfg.CONF.import_opt('auditlog_connection','auditlog.storage',group="database")
 
 class MongoDBStorage(base.StorageEngine):
     """Engine to store data into mongodb."""
 
     def get_connection(self, conf):
-	return Connection(conf)	 
+        return Connection(conf)
 
 
 class Connection(base.Connection):
     """MongoDB Connection."""
 
     def __init__(self, conf):
-	
-	self._pool = {}
-	self.url = conf.database.auditlog_connection
-	self.conn = self.connect(self.url)
-	connection_options = pymongo.uri_parser.parse_uri(self.url)
-	self.db = getattr(self.conn, connection_options['database'])
 
-    def connect(self,url):
+        self._pool = {}
+        self.url = conf.database.auditlog_connection
+        self.conn = self.connect(self.url)
+        connection_options = pymongo.uri_parser.parse_uri(self.url)
+        self.db = getattr(self.conn, connection_options['database'])
 
-	connection_options = pymongo.uri_parser.parse_uri(url)
+    def connect(self, url):
+
+        connection_options = pymongo.uri_parser.parse_uri(url)
         del connection_options['database']
         del connection_options['username']
         del connection_options['password']
@@ -41,22 +43,22 @@ class Connection(base.Connection):
             client = self._pool.get(pool_key)()
             if client:
                 return client
-        splitted_url = network_utils.urlsplit(url)
-        client = pymongo.MongoClient(url,safe=True)
+        client = pymongo.MongoClient(url, safe=True)
         self._pool[pool_key] = weakref.ref(client)
-        return client	  
+        return client
 
     def upgrade(self):
         pass
 
     def create_auditlog(self, log):
-	self.db.auditlog.insert(log)
+        self.db.auditlog.insert(log)
 
     def get_auditlog_by_id(self, id):
-	self.db.auditlog.find({"_id":"ObjectId("+id+")"})
+        self.db.auditlog.find({"_id": "ObjectId(" + id + ")"})
 
     def get_auditlogs_paginated(self, q, paginator,
                                 order_by=[], order_dir='asc'):
         pass
+
     def validate_query(self, q):
         pass
