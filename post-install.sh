3ff4941618babb7a787587b2829dac5687517dc9
xg.song <samsong8610@gmail.com>
2014-07-08 13:59:09 +0800

    Packing man page into rpm package
    Install man page into /usr/share/man/man1, although it is in Chinese.
    
    ref #419

--- post-install.sh
+++ post-install.sh
@@ -3,8 +3,13 @@ DOC_PATH=/usr/share/doc/%{name}-%{unmangled_version}
 [ -d /etc/auditlog/ ] || mkdir /etc/auditlog
 [ -d /var/log/auditlog/ ] || mkdir /var/log/auditlog
 [ -d /var/run/auditlog/ ] || mkdir /var/run/auditlog
+[ -d /usr/share/man/man1 ] || mkdir -p /usr/share/man/man1
 [ -f /etc/auditlog/auditlog.conf ] || cp ${DOC_PATH}/auditlog.conf /etc/auditlog/
 [ -f /etc/auditlog/policy.json ] || cp ${DOC_PATH}/policy.json /etc/auditlog/
 [ -f /etc/init.d/auditlog-api ] || cp ${DOC_PATH}/auditlog-api /etc/init.d/
+if [ ! -f /usr/share/man/man1/auditlog.1.gz ]; then
+   cp ${DOC_PATH}/auditlog.1 /usr/share/man/man1
+   gzip /usr/share/man/man1/auditlog.1
+fi
 chmod +x /etc/init.d/auditlog-api
 chkconfig --add auditlog-api
